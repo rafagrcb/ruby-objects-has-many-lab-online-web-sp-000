154693b04e845c8668872b9e820df6479347bd64
Rafael Garcia <rafagrcb@gmail.com>
2020-08-30 22:19:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,24 +7,24 @@ class Artist
     @name = name
   end
 
-  def self.song_count
+  def self.songs_count
     @@song_count += 2
   end
   
   def songs
-    song.all.select do |songs|
+    songs.all.select do |songs|
       #binding.pry
       song.artist == self
     end
   end
 
-  def add_songs(song_name)
-    song_name.artist = self
+  def add_songs(songs_name)
+    songs_name.artist = self
   end
 
-  def add_songs_by_name(song_name)
+  def add_songs_by_name(songs_name)
     #binding.pry
-    singer = Song.new(song_name)
+    singer = Song.new(songs_name)
     add_songs(singer)
   end
 end
