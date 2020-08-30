bdccf630196e40ac562dbcba896d92129fa71c75
Rafael Garcia <rafagrcb@gmail.com>
2020-08-30 22:29:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,32 +1,31 @@
 class Artist
   attr_accessor :name
 
-  @@song_count = 1
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
-  def self.song_count
-    @@song_count += 2
-  end
-  
-  def song
-    song.all.select do |song|
-      #binding.pry
-      song.artist == self
-    end
+  def self.all
+    @@all
   end
 
   def add_song(song)
-    @song << song
     song.artist = self
-    @@song_count += 1
   end
 
-  def add_song_by_name(song_name)
-    #binding.pry
-    singer = Song.new(song_name)
-    add_song(singer)
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count
   end
 end
